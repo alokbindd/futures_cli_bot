3d029b083004261dbbc8cc51f38518b74cc3f62e
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-04-16 14:30:24 +0530

    testing place_order function with error handling for market and limit orders

--- bot/orders.py
+++ bot/orders.py
@@ -1,1 +1,25 @@
 
+def place_order(client, symbol, side, order_type, quantity, price=None):
+    try:
+        if order_type == "MARKET":
+            order = client.futures_create_order(
+                symbol=symbol,
+                side=side,
+                type="MARKET",
+                quantity=quantity
+            )
+        elif order_type == "LIMIT":
+            order = client.futures_create_order(
+                symbol=symbol,
+                side=side,
+                type="LIMIT",
+                quantity=quantity,
+                price=price,
+                timeInForce="GTC"
+            )
+            
+        return order
+    except Exception as e:
+            print("Error", e)
+            raise
+
