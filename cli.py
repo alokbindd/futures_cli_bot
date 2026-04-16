b7d54378d16a0c8612e6dff5e6178b4ac378a6df
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-04-16 21:11:32 +0530

    Update CLI to allow optional symbol input and add interactive mode for missing arguments

--- cli.py
+++ cli.py
@@ -10,13 +10,23 @@ def main():
 
     parser = argparse.ArgumentParser(description="Binance Futures Trading Bot CLI")
 
-    parser.add_argument("--symbol", required=True, help="Trading pair symbol (e.g., BTCUSDT)")
+    parser.add_argument("--symbol", required=False, help="Trading pair symbol (e.g., BTCUSDT)")
     parser.add_argument("--side", required=True, help="Order side: BUY or SELL")
     parser.add_argument("--type", required=True, help="Order type: MARKET or LIMIT")
     parser.add_argument("--quantity", required=True, help="Order quantity (e.g., 0.001)")
     parser.add_argument("--price", required=False, help="Price required for LIMIT orders (e.g., 60000)")
     
     args = parser.parse_args()
+
+    if not all([args.symbol, args.side, args.type, args.quantity]):
+        print("Missing arguments. Switching to interactive mode...\n")
+
+        args.symbol = input("Enter Symbol: ")
+        args.side = input("Enter side (BUY/SELL): ")
+        args.type = input("Enter type (MARKET/LIMIT): ")
+        args.quantity = input("Enter quantity: ")
+        if args.type.upper() == "LIMIT": 
+            args.price = input("Enter Price: ")
 
     try:
         validate_order(
@@ -38,25 +48,39 @@ def main():
             print(f"Price: {args.price}")
 
         logging.info(f"Order Request: {vars(args)}")
-        
-        order = place_order(
-            client,
-            args.symbol.upper(),
-            args.side.upper(),
-            args.type.upper(),
-            args.quantity,
-            args.price,
-        )
-        print("\n-------------------------------")
-        print("\nOrder successful!")
-        print(f"Order ID: {order['orderId']}")
-        print(f"Symbol: {order['symbol']}")
-        print(f"Status: {order['status']}")
-        print(f"Executed Qty: {order['executedQty']}")
-        if "avgPrice" in order:
-            print(f"Average Price: {order['avgPrice']}")
-        
-        logging.info(f"Order Response: {order}")
+
+        while True:
+            confirmation = input("Are you sure you want to place this order? (y/n):").upper() 
+            if confirmation in ["Y","N"]:
+                break
+            else:
+                print("Enter a valid input! (y/n)")
+
+        if confirmation == "Y":        
+            order = place_order(
+                client,
+                args.symbol.upper(),
+                args.side.upper(),
+                args.type.upper(),
+                args.quantity,
+                args.price,
+            )
+            print("\n-------------------------------")
+            print("\nOrder Placed successfully")
+            print(f"Order ID: {order['orderId']}")
+            print(f"Symbol: {order['symbol']}")
+            print(f"Status: {order['status']}")
+            print(f"Executed Qty: {order['executedQty']}")
+            if "avgPrice" in order:
+                print(f"Average Price: {order['avgPrice']}")
+                
+            logging.info(f"Order Response: {order}")
+
+        elif confirmation == "N":
+            print("Order cancelled by user")
+            logging.info(f"Order cancelled by user")
+
+
     except Exception as e:
         print("\n Error:", str(e))
         logging.error(f"Error: {str(e)}")
